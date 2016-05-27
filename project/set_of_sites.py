a4b5be7075ad27f968ea44eb95bfa309d0c6ccd8
Georgie Wellock <glw33@chmc-7604.bath.ac.uk>
2016-05-27 16:34:44 +0100

    added a set of sites class which contains the functions to calculate rho, defect density, probability on full grid

--- project/set_of_sites.py
+++ project/set_of_sites.py
@@ -1,49 +1,31 @@
-from project.site import Site
 import numpy as np
+from project.practice import index_of_grid_at_x, phi_at_x
 from bisect import bisect_left
 
 class Set_of_Sites:
-    def __init__( self, grid ):
-        self.grid = grid
+    def __init__( self, sites ):
+        self.sites = sites
+    
+    def __add__( self, other ):
+        if type( other ) is not Set_of_Sites:
+            raise TypeError
+        return Set_of_Sites( self.sites + other.sites )
 
-    def phi_at_x( self, x, phi ):
-        index = self.index_of_grid_at_x( x )
-        return phi[ index ]
+    def calculate_rho( self, grid, phi, temp ):
+        rho = np.zeros_like( grid )
+        for site in self.sites:
+            rho[index_of_grid_at_x( grid, site.x )] += site.charge_density( phi_at_x( phi, grid, site.x ), temp )
+        return rho 
 
-    def index_of_grid_at_x( self, x ):
-        return closest_index( self.grid, x )
+    def calculate_probabilities( self, grid, phi, temp ):
+        probability = np.zeros_like( grid )
+        for site in self.sites:
+            probability[index_of_grid_at_x( grid, site.x )] = np.asarray( site.probabilities( phi_at_x( phi, grid, site.x ), temp ) )
+        return probability 
 
-    def sort_xy_data( array ):
-        ind = np.lexsort( ( array[:,1], array[:,0] ) )
-        return array[ ind ]
+    def calculate_defect_density( self, grid, phi, temp ):
+        defect_density = np.zeros_like( grid )
+        for site in self.sites:
+            defect_density[index_of_grid_at_x( grid, site.x )] += np.asarray(site.probabilities( phi_at_x( phi, grid, site.x ), temp )) * site.density
+        return defect_density  
 
-    def calculate_rho( self, sites, phi, temp ):
-        rho = np.zeros_like( self.grid )
-        for site in sites:
-            rho[self.index_of_grid_at_x( site.x )] += site.charge_density( self.phi_at_x( phi, site.x ), temp )
-        return rho
-
-    def calculate_defect_density( self, sites, phi, temp ):
-        defect_density = np.zeros_like( self.grid )
-        for site in sites:
-            defect_density[self.index_of_grid_at_x( site.x )] += np.asarray(site.probabilities( self.phi_at_x( phi, site.x ), temp )) * site.density
-        return defect_density
-
-def closest_index(myList, myNumber):
-    """
-    Assumes myList is sorted. Returns index of closest value to myNumber.
-
-    If two numbers are equally close, return the index of the smallest number.
-    """
-    print( myList, myNumber )
-    pos = bisect_left(myList, myNumber)
-    if pos == 0:
-        return 0
-    if pos == len(myList):
-        return len(myList)
-    before = myList[pos - 1]
-    after = myList[pos]
-    if after - myNumber < myNumber - before:
-       return pos
-    else:
-       return pos - 1
