755954b150dbfb5b71000e47e5e24e82580f19ca
Georgie Wellock <glw33@chmc-7604.bath.ac.uk>
2016-11-14 13:00:22 +0000

    updated laplacian

--- project/solver.py
+++ project/solver.py
@@ -15,9 +15,9 @@ class Solver:
         extended_deltax = np.insert( deltax, (0, len(deltax)), (mean_deltax, mean_deltax) )
         diag = -(extended_deltax[:n_points] + extended_deltax[1:])
         if neumann_bc[0]:
-            diag[0] -= deltax[0]
+            diag[0]  += deltax[0]
         if neumann_bc[1]:
-            diag[-1]-= deltax[-1]
+            diag[-1] += deltax[-1]
         ldiag = extended_deltax[:n_points-1]
         udiag = extended_deltax[2:]
         prefactor = 2.0 / ( extended_deltax[:n_points]* extended_deltax[1:] * (extended_deltax[:n_points] + extended_deltax[1:]) )
@@ -25,25 +25,25 @@ class Solver:
         L = (A.T * prefactor).T
         return L
 
-
-
-    def laplacian( self, neumann_bc=[False, False] ):
+    def laplacian_new_fullmatrix( self, neumann_bc=[False, False] ):
         n_points = len( self.grid )
         deltax = self.grid[1:] - self.grid[:-1]
         mean_deltax = np.mean( deltax )
-        extended_deltax = np.insert( deltax, (0, len(deltax)), (mean_deltax, mean_deltax) )
-        diag = -(extended_deltax[:n_points] + extended_deltax[1:])
+        delta_x1 = np.insert( deltax, 0, mean_deltax )
+        delta_x2 = np.insert( deltax, len(deltax), mean_deltax )
+        diag = -2.0 / (delta_x1 * delta_x2)
         if neumann_bc[0]:
-            diag[0] -= deltax[0]
+            diag[0]  += deltax[0]
         if neumann_bc[1]:
-            diag[-1]-= deltax[-1]
-        ldiag = extended_deltax[:n_points-1]
-        udiag = extended_deltax[2:]
-        prefactor = 2.0 / ( extended_deltax[:n_points]* extended_deltax[1:] * (extended_deltax[:n_points] + extended_deltax[1:]) )
-        A = diags( [ diag, udiag, ldiag ], [ 0, 1, -1 ] ).A
-        L = (A.T * prefactor).T
-#        return L
-        L_sparse = csc_matrix(L)
+            diag[-1] += deltax[-1]
+        ldiag = 2.0 / ( ( delta_x1 + delta_x2 ) * delta_x1 )
+        udiag = 2.0 / ( ( delta_x1 + delta_x2 ) * delta_x2 ) 
+        A = diags( [ diag, udiag[:-1], ldiag[1:] ], [ 0, 1, -1 ] ).A
+        return A
+
+
+    def laplacian_sparse( self, neumann_bc=[False, False] ):
+        L_sparse = csc_matrix( self.laplacian_new_fullmatrix( neumann_bc ) )
         return L_sparse
 
 
@@ -64,21 +64,8 @@ class Solver:
         predicted_phi = np.linalg.solve( A, b )
         return predicted_phi
 
-    def pb_solver( self, rho, boundary_condition, A, dielectric ):
+    def pb_solver_sparse( self, rho, boundary_condition, A, dielectric ):
         b = -( rho / self.epsilon( dielectric ) + boundary_condition )
-#        predicted_phi = np.linalg.solve( A, b )
         predicted_phi = linalg.spsolve( A, b )
         return predicted_phi
 
-    def regular_laplacian( self ):
-        n_points = len( self.grid )     
-        deltax = self.grid[1:] - self.grid[:-1]
-        mean_deltax = np.mean( deltax )
-        #extended_deltax = np.insert( deltax, (0, len(deltax)), (mean_deltax, mean_deltax) )
-        prefactor = 1.0/mean_deltax**2
-        diag = np.zeros( n_points ) + 2
-        udiag = np.zeros( n_points ) - 1
-        ldiag = np.zeros( n_points ) - 1     
-        A = diags( [ diag, udiag, ldiag ], [ 0, 1, -1 ] ).A
-        L = (A.T * prefactor).T
-        return L
