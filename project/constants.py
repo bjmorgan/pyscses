f63f20baea78ba338a60fc8b049128c58de0ef8a
glw33 <glw33@chmc-7604.bath.ac.uk>
2017-08-01 18:41:07 +0100

    All constants now read from scipy.constants

--- project/constants.py
+++ project/constants.py
@@ -1,11 +1,9 @@
-gas_const = 8.3144598
-faraday_const = 96485.33289
-n_a = 6.023e23
-boltzmann_const = gas_const / n_a
-fundamental_charge = faraday_const / n_a
-boltzmann_eV = boltzmann_const / fundamental_charge
+from scipy.constants import k, N_A, epsilon_0, elementary_charge, physical_constants
 
-vacuum_permittivity = 8.854187817e-12
+fundamental_charge = elementary_charge
+boltzmann_eV = physical_constants[ 'Boltzmann constant in eV/K' ][0]
+
+vacuum_permittivity = epsilon_0
 dielectric = 55
 
 
