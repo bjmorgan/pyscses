8bf6f79ce0f883a20b0c0a9f668a80851a26aac6
Benjamin Morgan <benmorgan2@gmail.com>
2017-10-04 11:24:28 +0100

    Defect_Species class renamed to DefectSpecies

--- project/defect_species.py
+++ project/defect_species.py
@@ -1,7 +1,7 @@
-class Defect_Species:
-    """ The Defect_Species class includes the information about each defect species present in the system. 
+class DefectSpecies:
+    """ The DefectSpecies class includes the information about each defect species present in the system. 
         label refers to what the defect is call ie 'Vo' for an oxygen vacancy. The valence is the charge of the defect and the mole_fraction is the mole fraction of the defect present in the system. fixed refers to whether the system is following a Mott-Schottky (immobile dopant ions) or Gouy Chapman ( mobile dopant ions ) approximation."""
-    def __init__( self, label, valence, mole_fraction, fixed = False ):
+    def __init__( self, label, valence, mole_fraction, fixed = False, mobility = 0.0 ):
         assert( isinstance( label, str ) )
         assert( isinstance( valence, float ) )
         assert( isinstance( mole_fraction, float ) )
@@ -9,3 +9,4 @@ class Defect_Species:
         self.valence = valence
         self.mole_fraction = mole_fraction
         self.fixed = fixed
+        self.mobility = mobility
