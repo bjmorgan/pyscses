15ab240b4dd352bfdadb59ce4cba1db830c5b29f
Geogie Wellock <glw33@bath.ac.uk>
2018-12-15 22:14:28 +0000

    readded mathjax to conf.py as mathimg didnt work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.imgmath'
+    'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
