9d9866f91378ffd96f411d4e9eba88c373f4ef25
Geogie Wellock <glw33@bath.ac.uk>
2018-12-15 22:02:14 +0000

    added mathjax to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphnix.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
