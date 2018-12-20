1b4c5d58d23190f3ab9f92010075b9176be9f2d6
Benjamin Morgan <benmorgan2@gmail.com>
2018-12-20 10:32:46 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-import os
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
+from pyscses import __version__ as VERSION
 
 readme = 'README.md'
+long_description = open( readme ).read()
 
 config = {
+    'description': 'PYthon Space-Charge Site Explicit Solver',
     'name': 'pyscses',
     'author': 'Georgina L. Wellock',
     'packages': ['pyscses'],
-    'install_requires': [ 'scipy', 'sympy', 'numpy' ]
+    'url': 'https://github.com/georgiewellock/pyscses',
+    'download_url': 'https://github.com/georgiewellock/pyscses/archive/%s.tar.gz' % (VERSION),
+    'version': VERSION,
+    'install_requires': open( 'requirements.txt' ).read(),
+    'license': 'MIT'
 }
 
 setup(**config)
