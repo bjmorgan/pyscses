39afbe4df18f26083784d8eef3b6f0bab376bd14
Benjamin Morgan <benmorgan2@gmail.com>
2021-07-08 11:06:50 +0000

    Added SiteData.as_input_line() method.

--- pyscses/site_data.py
+++ pyscses/site_data.py
@@ -6,8 +6,7 @@ class DefectData(object):
     def __init__(self,
                  label: str,
                  energy: float) -> None:
-        """
-        Instantiate a DefectData object
+        """Instantiate a DefectData object
 
         Args:
             label (str): Label for this defect species.
@@ -27,8 +26,7 @@ class SiteData(object):
                  valence: float,
                  x: float,
                  defect_data: Tuple[DefectData]) -> None:
-        """
-        Instantiate a SiteData object.
+        """Instantiate a SiteData object.
 
         Args:
             label (str): Site label used to reference this site.
@@ -43,3 +41,18 @@ class SiteData(object):
         self.valence = valence
         self.x = x
         self.defect_data = defect_data
+
+    def as_input_line(self) -> str:
+        """Returns a formatted string equivalent to the expected line
+        in an input file.
+
+        Args:
+            None
+
+        Returns:
+            str
+
+        """
+        defect_string = " ".join([f"{d.label} {d.energy}" for d in self.defect_data])
+        input_line_string = f"{self.label} {self.valence} {self.x} {defect_string}"
+        return input_line_string
--- tests/test_site_data.py
+++ tests/test_site_data.py
@@ -32,6 +32,26 @@ class TestSiteDataInit(unittest.TestCase):
         self.assertEqual(site_data.x, x)
         self.assertEqual(site_data.defect_data, defect_data)
 
+class TestSiteData(unittest.TestCase):
+
+    def setUp(self):
+        label = 'A'
+        valence = -2.0
+        x = 1.2345
+        defect_data = (DefectData(label='B',
+                                  energy=-1.0),
+                       DefectData(label='C',
+                                  energy=+1.0))
+        self.site_data = SiteData(label=label,
+                                  valence=valence,
+                                  x=x,
+                                  defect_data=defect_data)
+
+    def test_as_input_line(self):
+        expected_string = "A -2.0 1.2345 B -1.0 C 1.0"
+        self.assertEqual(self.site_data.as_input_line(),
+                         expected_string)
+
 
 if __name__ == '__main__':
     unittest.main()
