a3f8e916aa25c3070f4443c790a971341338a0d7
Benjamin Morgan <benmorgan2@gmail.com>
2021-07-09 15:19:27 +0000

    WIP calculating grid offsets.

--- pyscses/set_up_calculation.py
+++ pyscses/set_up_calculation.py
@@ -8,6 +8,33 @@ from bisect import bisect_left, bisect_right
 from sklearn.cluster import AgglomerativeClustering # type: ignore
 from typing import Tuple, List, Dict
 
+def format_line(line,
+    site_charge,
+    offset = 0.0):
+    """
+    Each line in the input file is formatted into separate site properties.
+    Args:
+    line (str): A line in the input file.
+    site_charge (bool): The site charge refers to the contribution to the overall charge of a site given by the original, non-defective species present at that site. True if the site charge contribution is to be included in the calculation, False if it is not to be included.
+    Returns:
+    list: line from the input file, split into individual values.
+    """
+    # x coordinate
+    if site_charge == True:
+        line[1] = float( line[1] )
+    if site_charge == False:
+        line[1] = 0.0
+    line[2] = float( line[2] )
+    # defect energies
+    for i in range( 4, len(line) ):
+        line[i] = float( line[i] )
+        if line[i] < 0.0:
+            line[i] += offset
+        if line[i] > 0.0:
+            line[i] += offset
+    #        line[i] = 0.0
+    return line
+
 def sites_data_from_file(filename: str,
                          x_limits: Tuple[float, float],
                          clustering_threshold: float = 1e-10,
@@ -37,6 +64,7 @@ def sites_data_from_file(filename: str,
     with open(filename, 'r') as f:
         input_data = [line.strip() for line in f.readlines() if line]
     # Validate the input data:
+    print(len(input_data))
     for line_number, line in enumerate(input_data, 1):
         if not SiteData.input_string_is_valid_syntax(line):
             raise InputFormatError(f"Input format error at line number {line_number} in file {filename}")
@@ -44,15 +72,18 @@ def sites_data_from_file(filename: str,
     sites_data = [SiteData.from_input_string(line,
                                              validate_input=False) # We have already validated the input above.
                   for line in input_data]
+    print(len(sites_data))
     # 1. Exclude data for sites with x coordinates outside the specified limits
     sites_data = [sd for sd in sites_data
                   if ((sd.x >= x_limits[0]) and
                       (sd.x < x_limits[1]))]
     # 2. Sort sites data by x coordinate.
     sites_data = sorted(sites_data, key=lambda sd: sd.x)
+    print(len(sites_data))
     # 3. Cluster sites data with similar x coordinates.
     cluster_similar_sites_data(sites_data=sites_data,
                                distance_threshold=1e-10)
+    print(len(sites_data))
     return sites_data
 
 def cluster_similar_sites_data(sites_data: List[SiteData],
@@ -88,10 +119,15 @@ def cluster_similar_sites_data(sites_data: List[SiteData],
             sd.x = mean_x
     return sites_data
 
-def calculate_grid_offsets(filename,
-                           x_min,
-                           x_max,
-                           system):
+def calculate_grid_offsets_from_sites_data(sites_data: List[SiteData],
+                                          x_limits: Tuple[float, float]) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+    unique_x_coords = np.unique([sd.x for sd in sites_data])
+    print(unique_x_coords)
+
+def calculate_grid_offsets(filename: str,
+                           x_min: float,
+                           x_max: float,
+                           system: str):
     """Reads in the input data calculates the distance to the next site outside of the defined calculation region. Allows calculation of the delta_x and volume values for the endmost grid points.
 
     Args:
@@ -104,14 +140,17 @@ def calculate_grid_offsets(filename,
         list, list: distance between the midpoint of the endmost sites and the midpoint of the next site outside of the calculation region for the first and last sites respectively. Distance between the endmost sites and the next site outside of the calculation region for the first and last sites respectively.
 
     """
-    with open( filename, 'r' ) as f:
-        input_data = [ line.split() for line in f ]
-        input_data = [ format_line( line, 0.0, 0.0 ) for line in input_data ]
+    with open(filename, 'r') as f:
+        input_data = [line.split() for line in f]
+        input_data = [format_line(line, 0.0, 0.0) for line in input_data]
         input_data = sorted( input_data, key=lambda x: x[2] )
         input_data = cluster_similar_sites( input_data )
         x_coords = np.unique( np.array( [ float(l[2]) for l in input_data ] ) )
+        print(len(x_coords))
+        print(x_coords)
         min_index = bisect_left( x_coords, x_min )
         max_index = bisect_right( x_coords, x_max )
+        print(min_index)
         min_offset = ((x_coords[min_index+1]-x_coords[min_index])/2)+((x_coords[min_index]-x_coords[min_index-1])/2)
         max_offset = ((x_coords[max_index+1]-x_coords[max_index])/2)+((x_coords[max_index]-x_coords[max_index-1])/2)
         if system == 'single':
@@ -121,4 +160,32 @@ def calculate_grid_offsets(filename,
             limits = [min_offset, min_offset]
             limits_for_laplacian = [ (x_coords[min_index]-x_coords[min_index-1]), (x_coords[min_index]-x_coords[min_index-1]) ]
     return limits, limits_for_laplacian
+
+def cluster_similar_sites(input_data):
+    """Clusters data points the input data to be projected onto a site. Clustering criterion is set as 0.01 nm.
+
+        Args:
+        input_data (str): The input file.
+
+        Returns:
+        str: The input file, formatted with x coordinate values with correct clustering coordinates.
+
+        """
+    coordinates = np.array([[ion[2]] for ion in input_data]) # format data into correct format
+    # Use sklearn's clustering functionality to assign site clusters
+    cluster = AgglomerativeClustering(n_clusters=None, affinity='euclidean', compute_full_tree = True,linkage='ward',distance_threshold = 1e-10)
+    cluster_data = cluster.fit_predict(coordinates)
+    # Match clusters to sites
+    sites = [ [] for i in range(0,np.max(cluster_data)+1)]
+    for cluster in range(len(cluster_data)):
+        sites[cluster_data[cluster-1]].append(input_data[cluster-1])
+    sites = sorted(sites,key = lambda x: x[0][2])
+    # Prepare data in the correct format.
+    sites_format = []
+    for cluster in sites:
+        site_loc = np.mean(np.array(cluster)[:,2].astype('float64'))
+        for ion in cluster:
+            ion[2] = site_loc
+        sites_format += cluster
+    return sites_format
 
